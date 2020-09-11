68e6788fbe6c2023b974ccaccde72ce5d89cc388
tageh <tage.hisdal@gmail.com>
2020-09-11 13:25:47 +0200

    edits

--- labOblig/oblig2/oblig2.c
+++ labOblig/oblig2/oblig2.c
@@ -5,6 +5,10 @@
 int lesInn(void);
 void trekk(int arr[]);
 void skrivUt(int arr[], int count);
+void selectionSort(int arr[], int n);
+void swap(int *xp, int *yp);
+int randomTall(void);
+
 
 int main(){
     int j;
@@ -12,10 +16,14 @@ int main(){
     int rekker = lesInn();
     int arrTall[7];
     srand(time(0));
+    int n = sizeof(arrTall)/sizeof(arrTall[0]); 
+    
+
 
     for(j = 0; j <rekker; j++){
         trekk(arrTall);
         printf("\n");
+        selectionSort(arrTall, n); 
         skrivUt(arrTall, j);
     }
     return 0;
@@ -28,10 +36,28 @@ int lesInn(void){
     return rekker;
 }
 
+int randomTall(void){
+    return (rand() % 34) + 1;
+}
+
 void trekk(int arr[]){
     int i;
-    for(i = 0; i<7; i++){
-        arr[i] = 1 + rand() % 34;
+    int j;
+    
+    for(i = 0; i < 7; i++){
+        int randTall = randomTall();
+        if(i == 0){
+            arr[0] = randTall;
+            continue;
+        }
+
+        for(j = 0; j < i; j++){
+            if(arr[j] == randTall){
+                randTall = randomTall();
+                j = 0;
+            }
+        }
+    arr[i] = randTall;
     }
 }
 
@@ -44,3 +70,23 @@ void skrivUt(int arr[], int count){
         printf("%d ",(arr[i]));
     }
 }
+
+void swap(int *xp, int *yp) { 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+
+void selectionSort(int arr[], int n) { 
+    int i, j, minste; 
+  
+    for (i = 0; i < n-1; i++) 
+    { 
+        minste = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[minste]) 
+            minste = j; 
+ 
+        swap(&arr[minste], &arr[i]); 
+    } 
+} 
